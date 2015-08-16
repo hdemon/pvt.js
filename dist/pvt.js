82057deef39988858d04d35495c8f658c87b578b
Masami Yonehara <zeitdiebe@gmail.com>
2015-08-16 22:12:19 +0900

    modify

---
+++ dist/pvt.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _story = require("./story");
+
+var _story2 = _interopRequireDefault(_story);
+
+module.exports = {
+  Story: _story2["default"]
+};
---
+++ src/pvt.js
@@ -0,0 +1,5 @@
+import Story from "./story"
+
+module.exports = {
+  Story: Story
+}
